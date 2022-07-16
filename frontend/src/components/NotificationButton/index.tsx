1e15d4e3b8de7d2376581b84764b8849fa851804
Marcelo <marcelossoares@live.com>
2022-07-16 20:19:02 -0300

    Twilio sms

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,9 @@
+import icon from '../../assets/img/notification-icon.svg'
+function NotificationButton() {
+    return(<div class="dsmeta-red-btn">
+    <img src="notification-icon.svg" alt="Notificar" />
+  </div>)
+  }
+  
+  export default NotificationButton
+  
